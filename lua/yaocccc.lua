d7f22e7cf0de7e7efd9ca1df338b467f6e17c6e4
chenyc <flyforest@outlook.com>
2021-08-02 22:43:26 +0800

    add comment type

--- lua/yaocccc.lua
+++ lua/yaocccc.lua
@@ -1,8 +1,8 @@
 -- vim-lines
 
 -- vim-comment
-vim.g.vim_line_comments = { vim= '"', vimrc= '"', js= '//', ts= '//', java= '//', class= '//', c= '//', h= '//', go= '//', lua= '--' }
-vim.g.vim_chunk_comments = { js= {'/**', ' *', ' */'}, ts= {'/**', ' *', ' */'}, sh= {':<<!', '', '!'}, md= {'```', ' ', '```'} }
+vim.g.vim_line_comments = { vim = '"', vimrc = '"', js = '//', ts = '//', java = '//', class = '//', c = '//', h = '//', go = '//', lua = '--', proto = '//' }
+vim.g.vim_chunk_comments = { js = {'/**', ' *', ' */'}, ts = {'/**', ' *', ' */'}, sh = {':<<!', '', '!'}, md = {'```', ' ', '```'}, proto = {'/**', ' *', ' */'} }
 require'common'.set_maps({
     { 'n', '??', ':NToggleComment<cr>', {silent = true, noremap = true}},
     { 'v', '/', ':<c-u>VToggleComment<cr>', {silent = true, noremap = true}},
