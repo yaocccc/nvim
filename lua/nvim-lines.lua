7d9ff275262b4321e277af5df88acc031d064f60
chenyc <flyforest@outlook.com>
2021-08-04 11:50:57 +0800

    nvim-lines 透明主题

--- lua/nvim-lines.lua
+++ lua/nvim-lines.lua
@@ -1,7 +1,3 @@
-vim.g.line_powerline_enable = 1
-vim.g.line_nerdfont_enable = 1
-vim.g.line_unnamed_filename='~'
-vim.g.line_statusline_getters = {'v:lua.GitInfo', 'v:lua.CocErrCount', 'v:lua.GetFt'}
 function GitInfo()
     local branch = vim.g.coc_git_status or ''
     local diff = vim.b.coc_git_status or ''
@@ -16,3 +12,12 @@ function GetFt()
     local ft = vim.api.nvim_eval('&ft')
     return string.format(' %s ', string.len(ft) > 0 and ft or '~')
 end
+
+vim.g.line_powerline_enable = 0
+vim.g.line_nerdfont_enable = 1
+vim.g.line_unnamed_filename='~'
+vim.g.line_statusline_getters = {'v:lua.GitInfo', 'v:lua.CocErrCount', 'v:lua.GetFt'}
+vim.g.line_hl = { none = 'NONE', light = 'NONE', dark = 'NONE', ['break'] = '244' }
+vim.api.nvim_command('au VimEnter * hi VimLine_Light cterm=BOLD')
+vim.api.nvim_command('au VimEnter * hi CursorLine ctermbg=NONE')
+vim.api.nvim_command('au VimEnter * hi CursorLineNr ctermbg=NONE')
