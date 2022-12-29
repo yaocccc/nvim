648d8bb696eafdbf047c24e73554add13faa81da
chenyc <flyforest@outlook.com>
2022-12-29 11:33:43 +0800

    update

--- lua/pack/coc.lua
+++ lua/pack/coc.lua
@@ -54,8 +54,6 @@ function M.config()
         { 'n', '\\g', ":call coc#config('git.addGBlameToVirtualText',  !get(g:coc_user_config, 'git.addGBlameToVirtualText', 0)) | call nvim_buf_clear_namespace(bufnr(), -1, line('.') - 1, line('.'))<cr>", {silent = true} },
         { 'x', '=', 'CocHasProvider("formatRange") ? "<Plug>(coc-format-selected)" : "="', {silent = true, noremap = true, expr = true}},
         { 'n', '=', 'CocHasProvider("formatRange") ? "<Plug>(coc-format-selected)" : "="', {silent = true, noremap = true, expr = true}},
-
-
     })
 end
 
--- lua/pack/fzf.lua
+++ lua/pack/fzf.lua
@@ -2,12 +2,12 @@ local G = require('G')
 local M = {}
 
 function M.config()
-    G.g.fzf_preview_window = {'right:40%', 'ctrl-/'}
+    G.g.fzf_preview_window = {'right,40%,<50(down,50%)', 'ctrl-/'}
     G.g.fzf_commits_log_options = '--graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr"'
     G.g.fzf_layout = {
         window={
             width=0.9,
-            height=0.6
+            height=0.8
         }
     }
     G.cmd("com! -bar -bang Ag call fzf#vim#ag(<q-args>, fzf#vim#with_preview({'options': '--delimiter=: --nth=4..'}), <bang>0)")
