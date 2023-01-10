30e6c6c0cdca10563222991aec457f5cd9002ea6
chenyc <flyforest@outlook.com>
2023-01-10 16:05:42 +0800

    /home/chenyc to ~

--- lua/pack/markdown.lua
+++ lua/pack/markdown.lua
@@ -3,7 +3,7 @@ local M = {}
 
 function M.config()
     G.g.mkdp_browser = 'wyeb'
-    G.g.mkdp_markdown_css = '/home/chenyc/.config/nvim/colors/markdown.css'
+    G.g.mkdp_markdown_css = '~/.config/nvim/colors/markdown.css'
     G.g.mkdp_page_title = '${name}'
     G.g.mkdp_preview_options = { hide_yaml_meta = 1, disable_filename = 1 }
     G.g.mkdp_theme = 'dark'
