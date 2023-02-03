5dc7bb9a211551e39d1aff2d2582de0230360b38
chenyc <flyforest@outlook.com>
2023-02-03 13:32:01 +0800

    添加treesitter parser bootstrap

--- lua/pack/tree-sitter.lua
+++ lua/pack/tree-sitter.lua
@@ -1,69 +1,71 @@
 local G = require('G')
 local M = {}
-
 function M.parser_bootstrap()
-    local lang = G.api.nvim_eval('&ft')
-    local has_parser = require('nvim-treesitter.parsers').has_parser(lang)
+    local parsers = require("nvim-treesitter.parsers")
+    local lang = parsers.ft_to_lang(G.api.nvim_eval('&ft'))
+    local has_parser = parsers.has_parser(lang)
     if not has_parser then
-        G.cmd([[ try | silent! call execute('TSInstall ' . &ft) | catch | endtry ]])
+        G.cmd("try | call execute('TSInstall " .. lang .. "') | catch | endtry")
     end
 end
 
 function M.config()
     G.hi({
-        ["@variable"] = {fg="NONE"};
-        ["@function"] = {fg=32};
-        ["@function.call"] = {fg=32};
-        ["@operator"] = {fg=166};
-        ["@keyword.operator"] = {fg=166};
+        ["@variable"] = { fg = "NONE" };
+        ["@function"] = { fg = 32 };
+        ["@function.call"] = { fg = 32 };
+        ["@operator"] = { fg = 166 };
+        ["@keyword.operator"] = { fg = 166 };
 
-        ["@property"] = {fg=172};
-        ["@field"]= {fg=150};
-        ["@method"] = {fg=166};
-        ["@method.call"] = {fg=9};
-        ["@parameter"] = {fg=9};
+        ["@property"] = { fg = 172 };
+        ["@field"] = { fg = 150 };
+        ["@method"] = { fg = 166 };
+        ["@method.call"] = { fg = 9 };
+        ["@parameter"] = { fg = 9 };
 
-        ["@keyword"] = {fg=1};
-        ["@keyword.function"] = {fg=32};
-        ["@exception"] = {fg=32};
+        ["@keyword"] = { fg = 1 };
+        ["@keyword.function"] = { fg = 32 };
+        ["@exception"] = { fg = 32 };
 
-        ["@statement"] = {fg=166};
-        ["@special"] = {fg=172};
-        ["@comment"]= {fg=71,sp='italic'};
-        ["@include"] = {fg=1};
-        ["@type"] = {fg=179};
-        ["@type.builtin"] = {fg=150};
-        ["@punctuation.bracket"] = {fg=151};
+        ["@statement"] = { fg = 166 };
+        ["@special"] = { fg = 172 };
+        ["@comment"] = { fg = 71, sp = 'italic' };
+        ["@include"] = { fg = 1 };
+        ["@type"] = { fg = 179 };
+        ["@type.builtin"] = { fg = 150 };
+        ["@punctuation.bracket"] = { fg = 151 };
 
-        ["@constructor"] = {fg=172};
-        ["@namespace"] = {fg=172};
+        ["@constructor"] = { fg = 172 };
+        ["@namespace"] = { fg = 172 };
 
-        ["@string"] = {fg=37};
-        ["@number"] = {fg=37};
-        ["@boolean"] = {fg=37};
+        ["@string"] = { fg = 37 };
+        ["@number"] = { fg = 37 };
+        ["@boolean"] = { fg = 37 };
 
-        ["@tag"] = {fg=172};
+        ["@tag"] = { fg = 172 };
 
-        ["@text.literal"] = {fg=7};
+        ["@text.literal"] = { fg = 7 };
 
-        ["@text.todo.unchecked"] = {fg=172};
-        ["@text.todo.checked"] = {fg=37};
+        ["@text.todo.unchecked"] = { fg = 172 };
+        ["@text.todo.checked"] = { fg = 37 };
     })
     G.map({
-        { 'n', 'H', ':TSHighlightCapturesUnderCursor<CR>', {silent = true, noremap = true}},
-        { 'n', 'R', ':write | edit | TSBufEnable highlight<CR>', {silent = true, noremap = true}},
+        { 'n', 'H', ':TSHighlightCapturesUnderCursor<CR>', { silent = true, noremap = true } },
+        { 'n', 'R', ':write | edit | TSBufEnable highlight<CR>', { silent = true, noremap = true } },
     })
     -- some custom highlights
     G.cmd('match Todo /TODO\\(:.*\\)*/')
 end
 
 function M.setup()
-    require('nvim-treesitter.configs').setup {
-        ensure_installed = {},
+    require('nvim-treesitter.configs').setup({
+        -- 列举常用语言自动安装parser
+        ensure_installed = { 'typescript', 'javascript', 'vue', 'go', 'lua', 'markdown' },
         highlight = {
             enable = true
         },
-    }
+    })
+    M.parser_bootstrap()
     G.cmd([[ au FileType * lua require('pack/tree-sitter').parser_bootstrap() ]])
 end
 
