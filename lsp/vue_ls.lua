b0d695829f374f1c0502cce720bf91b78dac1b46
chenyc <flyforest@outlook.com>
2026-03-19 20:47:34 +0800

    unpack 2 table.unpack

--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -25,8 +25,8 @@ return {
                 return
             end
 
-            local param = unpack(result)
-            local id, command, payload = unpack(param)
+            local param = table.unpack(result)
+            local id, command, payload = table.unpack(param)
             ts_client:exec_cmd({
                 title = 'vue_request_forward',
                 command = 'typescript.tsserverRequest',
