33e4bb16c214181666178a34f0413f2f75e75edc
chenyc <flyforest@outlook.com>
2026-03-21 15:29:03 +0800

    添加 biome配置（原生不支持直接从lsp透传配置，所以从json指定）

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -2,4 +2,7 @@ return {
     cmd = { 'biome', 'lsp-proxy' },
     filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json', 'jsonc' },
     root_markers = { 'biome.json', 'biome.jsonc' },
+    settings = {
+        configurationPath = vim.fn.stdpath('config') .. '/biome.json',
+    }
 }
